f5b133772aa8561b6c24c9f155cbf9009b3ce8a8
Aman-Mandal <002amankumar@gmail.com>
2022-01-28 15:50:19 +0530

    Link added

--- src/BinarySearch/LeetCode_Qstns/FirstAndLastPosition.java
+++ src/BinarySearch/LeetCode_Qstns/FirstAndLastPosition.java
@@ -1,5 +1,7 @@
 package BinarySearch.LeetCode_Qstns;
 
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
 public class FirstAndLastPosition {
     public static void main(String[] args){
 
