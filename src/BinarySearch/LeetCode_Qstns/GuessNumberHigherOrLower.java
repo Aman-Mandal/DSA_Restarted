a934e0783bce3ada48f4363ec1e6056e48519eb4
Aman-Mandal <002amankumar@gmail.com>
2022-01-31 12:53:53 +0530

    Guess Number Higher or Lower - LeetCode

---
+++ src/BinarySearch/LeetCode_Qstns/GuessNumberHigherOrLower.java
@@ -0,0 +1,28 @@
+package BinarySearch.LeetCode_Qstns;
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+// There's an API in LeetCode for this question we have to implement that
+
+public class GuessNumberHigherOrLower {
+
+    int guessNumber(int n){
+        int start = 1;
+        int end = n;
+
+        while (start <= end){
+            int mid = start + (end - start)/ 2;
+            int guess = guess(mid);
+
+            if(guess == 1){
+                start = mid + 1;
+            }
+            else if(guess == -1){
+                end = mid - 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
