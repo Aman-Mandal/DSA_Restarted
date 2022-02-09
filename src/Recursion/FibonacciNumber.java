b217ea214efb2c3b6ed69fe29bea6845b3d42101
Aman-Mandal <002amankumar@gmail.com>
2022-02-09 18:55:57 +0530

    Fibonacci Number in Recursion

---
+++ src/Recursion/FibonacciNumber.java
@@ -0,0 +1,27 @@
+
+// A recursion consist of 2 things:
+// 1. Base Condition
+// 2. Recursive call
+
+package Recursion;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        System.out.println(fibo(7));
+    }
+
+    static int fibo(int n){
+
+        // base condition
+        if(n == 0){
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+
+        // recursive call
+        return fibo(n-1) + fibo(n-2);
+    }
+}
