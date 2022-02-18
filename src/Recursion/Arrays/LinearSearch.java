b8d61896a01a401f6bba334a9fe8453c4aa6dc63
Aman-Mandal <002amankumar@gmail.com>
2022-02-18 18:46:21 +0530

    ArrayList - Recursion

--- src/Recursion/Arrays/LinearSearch.java
+++ src/Recursion/Arrays/LinearSearch.java
@@ -1,10 +1,16 @@
 package Recursion.Arrays;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int [] arr = {1,3,5,7,12,-17,6};
+        int [] arr = {1,1,1,1,3,5,7,12,-17,6};
         int target = 1;
         System.out.println(findIndex(arr,target,0));
+        System.out.println(find(arr,target,0));
+        System.out.println(findIndexLast(arr,target,arr.length-1));
+        findAllIndex(arr, target, 0);
+        System.out.println(list);
     }
 
     // using boolean
@@ -41,5 +47,37 @@ public class LinearSearch {
         else {
             return findIndex(arr, target, index+1);
         }
+    }
+
+
+    // finding the last index ie. finding from the back side
+    static int findIndexLast(int[] arr, int target, int index){
+
+        // if index is -1 means element not found
+        if (index == -1) {
+            return -1;
+        }
+        else if(arr[index] == target){
+            return index;
+        }
+        // check in the remaining array for the target
+        else {
+            return findIndexLast(arr, target, index - 1);
+        }
+    }
+
+
+    // find all the index of the target
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex (int[] arr, int target, int index){
+
+        if(index == arr.length){
+            return;
+        }
+        // add the indexes in the list
+        if (arr[index] == target){
+            list.add(index);
+        }
+        findAllIndex(arr,target, index + 1);
     }
 }
