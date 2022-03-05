82906f91e92076ad84dad6f1c4884fbcfe1d0989
Aman-Mandal <002amankumar@gmail.com>
2022-03-05 20:13:48 +0530

    Recursion

--- src/BinarySearch/LeetCode_Qstns/PeakIndexMountainArray.java
+++ src/BinarySearch/LeetCode_Qstns/PeakIndexMountainArray.java
@@ -7,7 +7,7 @@ package BinarySearch.LeetCode_Qstns;
     1. We know its a Mountain Array, So we have to find the peak element.
     2. We will find the mid element and check whether the mid is greater than mid + 1.
     3. if arr[mid] > arr[mid + 1] it means we are in the descending part of the arr
-    4. so we will check the left side of mid for peak. ie. end = mid + 1
+    4. so we will check the left side of mid for peak. ie. end = mid
     5. if arr[mid] < arr[mid + 1] it means we are in the ascending part
     6. so we will check the right side for peak i.e start = mid + 1
     7. at last start and end both will point on the peal element ... so return end or start
